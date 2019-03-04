7fdfc4a83691dd03651136a198e28ce649d0700d
HaskeYE <yegor475@bk.ru>
2019-03-04 15:57:29 +0300

    First try(working Set ver with new access type and rehash suggestion)

--- src/main/java/CustomHashTable.java
+++ src/main/java/CustomHashTable.java
@@ -4,13 +4,15 @@ import com.sun.istack.internal.NotNull;
 
 import java.util.*;
 
+import static java.lang.Math.abs;
+// пакеты
 public class CustomHashTable {
 
 
     /*private static float DEFAULT_LOAD_FACTOR = 0.75f;*/
-    private HashMap<Integer, Set<Integer>> numerals;
+    private Map<Integer, Set<Integer>> numerals;
     private int hashDiv; //хеш-функция деления
-    private float loadFactor;
+    private int loadFactor;
     private int capacity;
 
 
@@ -32,19 +34,16 @@ public class CustomHashTable {
 
     //Getting bucket number
     private int hash(int numeral) {
-        return numeral % hashDiv;
+        return abs(numeral) % hashDiv;
     }
 
-    private int indexForHash(int hash) {
-        //Ддя возможно необходимых дальнейших операций
-        return hash;
-    }
 
     //Adding new numeral to HashTable
-    boolean add(int num) {
+    // область видимости
+    public boolean add(int num) {
         //Trying to realize if this numeral already in HashTable
-        if (!numerals.get(indexForHash(hash(num))).contains(num)) {
-            int index = indexForHash(hash(num));
+        if (!contains(num)) {
+            int index = hash(num);
             numerals.get(index).add(num);
             loadFactor++;
             if ((capacity * 0.75) <= loadFactor) {
@@ -55,23 +54,24 @@ public class CustomHashTable {
     }
 
     //Deleting numeral from HashTable
-    boolean delete(int num) {
-        Set<Integer> list = numerals.get(indexForHash(hash(num)));
+     public boolean delete(int num) {
+        Set<Integer> list = numerals.get(hash(num));
         if (list.contains(num)) {
             if (list.size() > 1) {
                 list.remove(num);
-                return true;
+                return true;               // duplication
             } else {
                 //Replacing set with one numeral by empty one
-                numerals.replace(indexForHash(hash(num)), new HashSet<>());
+                numerals.replace(hash(num), new HashSet<>()); //use-case
                 return true;
             }
         }
         return false;
     }
 
-    boolean contains(int num) {
-        return (numerals.get(indexForHash(hash(num))).contains(num));
+
+    public boolean contains(int num) {
+        return (numerals.get(hash(num)).contains(num));
     }
 
     @Override
@@ -81,19 +81,20 @@ public class CustomHashTable {
                 ((CustomHashTable) obj).hashDiv == hashDiv);
     }
 
-
+    // map
     void rehash() {
-        CustomHashTable table = new CustomHashTable(Collections.emptyList(),
-                (hashDiv * 2));
-        table.capacity = capacity * 2;
+        Map<Integer, Set<Integer>> table = new HashMap<>(hashDiv * 2);
         capacity = capacity * 2;
         for (int i = 0; i < hashDiv; i++) {
+            table.put(i, new HashSet<>());
+        }
+        for (int i = 0; i < hashDiv; i++) {
             for (Integer j : numerals.get(i)) {
-                table.add(j);
+                table.get(hash(j)).add(j);
             }
         }
         hashDiv = hashDiv * 2;
-        numerals = table.numerals;
+        numerals = table;
     }
     //REHASH
     /*double loadFactor = (1.0 * size) / numBuckets;
